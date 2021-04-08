5ca1f7f4d07117c36c4421d9750e727c7d058017
Emmanuel Egomson <fegomson@gmail.com>
2021-04-08 18:27:31 -0500

    Implemented the Tab Menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,6 +1,7 @@
-import React, { cloneElement } from 'react'
+import React, { cloneElement, useState } from 'react'
 import {AppBar, Toolbar, useScrollTrigger} from "@material-ui/core"
 import {makeStyles} from '@material-ui/styles'
+import {Tabs, Tab} from "@material-ui/core"
 
 import logo from "../../assets/logo.svg"
 
@@ -12,6 +13,17 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: "7em"
+  },
+  tabContainer: {
+    marginLeft: "auto",
+    marginRight: "1rem"
+  },
+  tab: {
+    textTransform: "none",
+    fontWeight: 700,
+    fontSize: "1rem",
+    minWidth: 10,
+    marginLeft: "25px"
   }
 }))
 
@@ -30,7 +42,13 @@ function ElevationScroll(props) {
 
 
 const Header = (props) => {
+  const [value, setValue] = useState(0)
   const classes = useStyles()
+
+  // Tabs Menu
+  const handleTabChange = (e, newValue) => {
+    setValue(newValue)
+  }
 
   return (
     <>
@@ -38,6 +56,13 @@ const Header = (props) => {
         <AppBar>
           <Toolbar disableGutters>
             <img src={logo} alt="Company Logo" className={classes.logo}/>
+            <Tabs value={value} onChange={handleTabChange} className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home"/>
+              <Tab className={classes.tab} label="Services"/>
+              <Tab className={classes.tab} label="The Revolution"/>
+              <Tab className={classes.tab} label="About Us"/>
+              <Tab className={classes.tab} label="Contact Us"/>
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
