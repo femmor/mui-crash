dba6c16c2a2a29736176b2c4b93f14d4057d651f
Emmanuel Egomson <fegomson@gmail.com>
2021-04-08 16:57:51 -0500

    Styling Themes - Colors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import Header from "./components/ui/Header"
+import {ThemeProvider} from "@material-ui/core/styles"
+import theme from "./components/ui/Theme"
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header/>
-      {[...new Array(50)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-              Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-              Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-    </div>
+    </ThemeProvider>
   );
 }
 
