d59b1f8d20aa1587d4aeba9289464b304afa70a1
Emmanuel Egomson <fegomson@gmail.com>
2021-04-08 16:33:58 -0500

    Implement AppBar Elevation

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import {AppBar, Toolbar} from "@material-ui/core"
+import React, { cloneElement } from 'react'
+import {AppBar, Toolbar, useScrollTrigger} from "@material-ui/core"
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  });
+
+  return cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 
 const Header = (props) => {
   return (
-    <AppBar>
-      <Toolbar>
-        Arc Development
-      </Toolbar>
-    </AppBar>
+    <>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar>
+            Arc Development
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+    </>
   )
 }
 
