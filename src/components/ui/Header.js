9c1b461ac62965cc319041070962c891fad25837
Emmanuel Egomson <fegomson@gmail.com>
2021-04-09 16:44:52 -0500

    Implement active tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { cloneElement, useState } from 'react'
+import React, { cloneElement, useState, useEffect } from 'react'
 import {AppBar, Button, Toolbar, useScrollTrigger} from "@material-ui/core"
 import {makeStyles} from '@material-ui/styles'
 import {Tabs, Tab} from "@material-ui/core"
@@ -11,6 +11,12 @@ const useStyles = makeStyles(theme => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
     marginBottom: "3em"
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
   },
   logo: {
     height: "7em"
@@ -57,13 +63,38 @@ const Header = (props) => {
     setValue(newValue)
   }
 
+  useEffect(() => {
+    if(window.location.pathname === "/" && value !== 0){
+      setValue(0)
+    } else if(window.location.pathname === "/services" && value !== 1){
+      setValue(1)
+    } else if(window.location.pathname === "/revolution" && value !== 2){
+      setValue(2)
+    } else if(window.location.pathname === "/about" && value !== 3){
+      setValue(3)
+    } else if(window.location.pathname === "/contact" && value !== 4){
+      setValue(4)
+    } else if(window.location.pathname === "/estimate" && value !== 5){
+      setValue(5)
+    }
+  }, [value])
+
   return (
     <>
       <ElevationScroll>
         <AppBar>
           <Toolbar disableGutters>
-            <img src={logo} alt="Company Logo" className={classes.logo}/>
-            <Tabs value={value} onChange={handleTabChange} className={classes.tabContainer}>
+            <div className="l0go-container">
+              <Button 
+                className={classes.logoContainer} 
+                component={Link} to="/" 
+                onClick={() => setValue(0)}
+                disableRipple
+                >
+                <img src={logo} alt="Company Logo" className={classes.logo}/>
+              </Button>
+            </div>
+            <Tabs value={value} onChange={handleTabChange} className={classes.tabContainer} indicatorColor="primary">
               <Tab className={classes.tab} component={Link} to="/" label="Home"/>
               <Tab className={classes.tab} component={Link} to="/services" label="Services"/>
               <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution"/>
